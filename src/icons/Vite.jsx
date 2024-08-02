ecd2f2517ac35a4f4dbade6adae9fc5e70546595
Francisco Rodriguez <fran11799@outlook.com>
2024-08-02 18:27:51 +0200

    fix(Root): Change vite icon

--- src/icons/Vite.jsx
+++ src/icons/Vite.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ViteIcon = () => {
   return (
     <div>
-      <img src="/src/icons/Vite.png" alt="ViteIcon" className="size-4" />
+      <img src="/assets/icons/vite.png" alt="ViteIcon" className="size-4" />
     </div>
   );
 };
