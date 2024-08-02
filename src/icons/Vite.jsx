ae0496799ff8038ad15256eb3f3eb34bd38519d3
Francisco Rodriguez <fran11799@outlook.com>
2024-08-02 17:39:09 +0200

    fix(Root): Change ViteIcon from astro to jsx component because there was on conflict with other icons, Idk why

---
+++ src/icons/Vite.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ViteIcon = () => {
+  return (
+    <div>
+      <img src="/src/icons/Vite.png" alt="ViteIcon" className="size-4" />
+    </div>
+  );
+};
+
+export default ViteIcon;
