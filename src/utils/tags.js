3beada79351f40ec8529b42135db5df32c349130
Francisco Rodriguez <fran11799@outlook.com>
2024-12-02 18:37:33 +0100

    Add skills section

--- src/utils/tags.js
+++ src/utils/tags.js
@@ -11,6 +11,15 @@ import MongoIcon from '@icons/Mongo.astro';
 import NodeIcon from '@icons/NodeJS.astro';
 import ViteIcon from '@icons/Vite';
 import ExpressIcon from '@icons/Express.astro';
+import DenoIcon from '@icons/Deno.astro';
+import DockerIcon from '@icons/Docker.astro';
+import GitIcon from '@icons/Git.astro';
+import GitHubIcon from '@icons/GitHub.astro';
+import GraphqlIcon from '@icons/Graphql.astro';
+import MySQLIcon from '@icons/MySQL.astro';
+import StripeIcon from '@icons/Stripe.astro';
+import TypeScriptIcon from '@icons/TypeScript.astro';
+import CppIcon from '@icons/Cpp.astro';
 
 export const TAGS = {
 	HTML: {
@@ -49,6 +58,12 @@ export const TAGS = {
 		link: 'https://developer.mozilla.org/es/docs/Web/JavaScript',
 		icon: JSIcon,
 	},
+	TYPESCRIPT: {
+		name: 'TypeScript',
+		class: 'bg-[#3178C6]/20 border-[#3178C6] text-[#3178C6]',
+		link: 'https://www.typescriptlang.org/',
+		icon: TypeScriptIcon,
+	},
 	REACT: {
 		name: 'React',
 		class: 'bg-[#00D8FF]/20 border-[#00D8FF] text-[#00D8FF]',
@@ -73,6 +88,12 @@ export const TAGS = {
 		link: 'https://nodejs.org/',
 		icon: NodeIcon,
 	},
+	DENO: {
+		name: 'Deno',
+		class: 'bg-[#fff]/20 border-[#fff] text-[#fff]',
+		link: 'https://deno.com/',
+		icon: DenoIcon,
+	},
 	VITE: {
 		name: 'Vite',
 		class: 'bg-[#BD34FE]/20 border-[#c436ff] text-[#c436ff]',
@@ -90,5 +111,47 @@ export const TAGS = {
 		class: 'bg-[#dbdbdb]/20 border-[#dbdbdb] text-[#dbdbdb]',
 		link: 'https://expressjs.com/',
 		icon: ExpressIcon,
+	},
+	DOCKER: {
+		name: 'Docker',
+		class: 'bg-[#008fe2]/20 border-[#008fe2] text-[#008fe2]',
+		link: 'https://www.docker.com/',
+		icon: DockerIcon,
+	},
+	GIT: {
+		name: 'Git',
+		class: 'bg-[#DE4C36]/20 border-[#DE4C36] text-[#DE4C36]',
+		link: 'https://git-scm.com/',
+		icon: GitIcon,
+	},
+	GITHUB: {
+		name: 'GitHub',
+		class: 'bg-[#fafbfc]/20 border-[#fafbfc] text-[#fafbfc]',
+		link: 'https://github.com/',
+		icon: GitHubIcon,
+	},
+	GRAPHQL: {
+		name: 'Graphql',
+		class: 'bg-[#e10098]/20 border-[#e10098] text-[#e10098]',
+		link: 'https://graphql.org/',
+		icon: GraphqlIcon,
+	},
+	MySQL: {
+		name: 'MySQL',
+		class: 'bg-[#008fb6]/20 border-[#008fb6] text-[#008fb6]',
+		link: 'https://www.mysql.com/',
+		icon: MySQLIcon,
+	},
+	STRIPE: {
+		name: 'Stripe',
+		class: 'bg-[#635BFF]/20 border-[#635BFF] text-[#635BFF]',
+		link: 'https://stripe.com/',
+		icon: StripeIcon,
+	},
+	CPP: {
+		name: 'C++',
+		class: 'bg-[#0075d2]/20 border-[#0075d2] text-[#0075d2]',
+		link: 'https://isocpp.org/',
+		icon: CppIcon,
 	},
 };
